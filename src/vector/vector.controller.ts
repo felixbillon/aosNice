734c03aab27fb1cb04fbdbaa6266f97f25b97511
spontoreau <pontoreau.sylvain@gmail.com>
2020-10-06 15:24:58 +0200

    :sparkles: Add Vector actions

--- src/vector/vector.controller.ts
+++ src/vector/vector.controller.ts
@@ -1,9 +1,40 @@
-import { Controller } from '@nestjs/common';
-import { VectorService } from './vector.service';
+import { Body, Controller, Post } from '@nestjs/common';
+import { Vector, VectorService } from './vector.service';
 
 @Controller('vector')
 export class VectorController {
-    constructor(
-        private vectorService: VectorService
-    ) { }
+  constructor(
+    private vectorService: VectorService
+  ) { }
+
+  @Post('add')
+  add(
+    @Body() vector1: Vector,
+    @Body() vector2: Vector
+  ) {
+    return this.vectorService.add(vector1, vector2);
+  }
+
+  @Post('substract')
+  substract(
+    @Body() vector1: Vector,
+    @Body() vector2: Vector
+  ) {
+    return this.vectorService.add(vector1, vector2);
+  }
+
+  @Post('round')
+  round(
+    @Body() vector1: Vector
+  ) {
+    return this.vectorService.round(vector1);
+  }
+
+  @Post('distance')
+  distance(
+    @Body() vector1: Vector,
+    @Body() vector2: Vector
+  ) {
+    return this.vectorService.distance(vector1, vector2);
+  }
 }
